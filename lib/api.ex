8e91b34acb69fe9657eb1113065555890e6db0a2
Waseem Awashra <wawashra@gmail.com>
2024-05-19 11:22:07 +0300

    clean the debug log

--- lib/api.ex
+++ lib/api.ex
@@ -157,8 +157,6 @@ defmodule ExForce.API do
       sf_sql =
         "SELECT #{encode_param_list(param_list)} FROM #{object} WHERE #{property_name} IN #{encode_property_values(property_values)}"
 
-      Logger.debug(sf_sql)
-
       case ExForce.query(
              client,
              sf_sql
