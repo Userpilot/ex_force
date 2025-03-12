541f867fcc02c44cc448982c03ccf418927297d7
AmrIDG <amr@userpilot.co>
2025-03-12 10:17:03 +0200

    Update api functions.ex

--- lib/api.ex
+++ lib/api.ex
@@ -501,6 +501,20 @@ defmodule ExForce.API do
     end
   end
 
+  def create_custom_field(app_token, schema) do
+    with {:ok, client} <- get_client(app_token),
+         %{config: %{access_token: _access_token}} = Salesforce.get_app(app_token) do
+      ExForce.create_custom_field(client, schema)
+    end
+  end
+
+  def bulk_create_sobjects(app_token, attrs) do
+    with {:ok, client} <- get_client(app_token),
+         %{config: %{access_token: _access_token}} = Salesforce.get_app(app_token) do
+      ExForce.bulk_create_sobjects(client, attrs)
+    end
+  end
+
   defp encode_param_list(param_list), do: Enum.join(param_list, " ,")
 
   defp encode_property_values([] = _property_values), do: "('')"
